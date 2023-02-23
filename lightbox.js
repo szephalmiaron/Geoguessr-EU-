234566eaf283e2d9c94d235591b68a9b7640abde
szephalmiaron <szephalmi.arn@students.jelik.eu>
2023-02-23 15:04:55 +0100

    olaszország hozzáadás

--- lightbox.js
+++ lightbox.js
@@ -1,10 +1,9 @@
 window.onload = () => {
-    // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
+    
     let all = document.getElementsByClassName("zoomD"),
         lightbox = document.getElementById("lightbox");
    
-    // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
-    // * SIMPLY CLONE INTO LIGHTBOX & SHOW
+    
     if (all.length>0) { for (let i of all) {
       i.onclick = () => {
         let clone = i.cloneNode();
@@ -15,6 +14,6 @@ window.onload = () => {
       };
     }}
    
-    // (C) CLICK TO CLOSE LIGHTBOX
+    
     lightbox.onclick = () => lightbox.className = "";
   };
