35f333ffd38ddf2139de07ed168e27929870a9a0
szephalmiaron <szephalmi.arn@students.jelik.eu>
2023-03-01 17:48:24 +0100

    quiz elkezdése

---
+++ quiz/game.js
@@ -0,0 +1,107 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'Melyik országban vannak elmosódások a térképen?',
+        choice1:'Némethország',
+        choice2:'Ausztria',
+        choice3:'Lengyelország',
+        choice4:'Spanyolország',
+        answer: 2,
+    },
+    {
+        question: 'Melyik ország fővárosa Tallinn?',
+        choice1:'Észtország',
+        choice2:'Horvátország',
+        choice3:'Finnország',
+        choice4:'Litvánia',
+        answer: 1,
+    },
+    {
+        question: 'Melyik országban használnak ciril betűket?',
+        choice1:'Románia',
+        choice2:'Bulgária',
+        choice3:'Lengyelország',
+        choice4:'Lettország',
+        answer: 2,
+    },
+    {
+        question: 'Melyik országban vezetnek a bal oldalon?',
+        choice1:'Görögország',
+        choice2:'Portugália',
+        choice3:'Málta',
+        choice4:'Svájc',
+        answer: 3,
+    },
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+
+    questionCounter = 0 
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    } 
+    
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+} 
+
+choices.forEach(choice =>{
+    choice.addEventListener('click', e =>{
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target 
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+
+            selectedChoice.parentElement.classlist.add(classToApply)
+
+            setTimeout(()=> {
+                selectedChoice.parentElement.classlist.remove(classToApply)
+                getNewQuestion()
+
+            }, 1000)
+        }
+    })
+})
